9bdb381c93acad44a092aa61a23ae61734ae602c
Gevorg Tsaturyan <you@example.com>
2019-09-28 16:06:03 +0400

    small fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 import Main from './LSinnovations/Main/main';
 const application = (
     <BrowserRouter>
@@ -12,7 +11,4 @@ const application = (
 
 ReactDOM.render(application, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
